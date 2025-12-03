3469e4dca005792ef3cb1bce4d9685a994e1a4c3
Matheus040209 <matheus_spilmam@estudante.sesisenai.org.br>
2025-12-03 17:23:49 -0300

    Inicio do exercicio 12

---
+++ Exercícios/JavaScript/Exercícios/Manipulação do DOM (inicial)/Exercício 12/script.js
@@ -0,0 +1,7 @@
+const btnRegistrar = document.getElementById ("btnRegistrar")
+const btnLimpar = document.getElementById ("btnLimpar");
+
+btnRegistrar.addEventListener("click", function(){
+    const registro = document.createElement ("p")
+    registro.textContent = "Clique registrado"
+})
